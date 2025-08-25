13f7804f549760110f9c6f62c3f79b732177bb8c
abdullahhas <abdullah312003@gmail.com>
2025-08-26 02:12:44 +0500

    setting up the backend and frontend folder

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import authRoutes from "./routes/authRoute.js"
+import "dotenv/config"
+
+
+const app = express()
+
+app.use("/api/auth" , authRoutes)
+
+
+const PORT = process.env.PORT
+
+
+
+app.listen(PORT , ()=> console.log(`Server is listening on ${PORT} `) )
+
