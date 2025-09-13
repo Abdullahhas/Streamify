6e7a7a1ba0e325c99c4ba04496d10058de8a1fd5
abdullahhas <abdullah312003@gmail.com>
2025-09-14 00:04:57 +0500

    connecting backend with frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,20 @@ import chatRoutes from "./routes/chatRoute.js"
 import { connectdb } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import "dotenv/config"
+import cors from "cors"
+
+
 
 
 const app = express()
 app.use(express.json());
 app.use(cookieParser())
+
+app.use(cors({
+    origin:["http://localhost:5173"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 
 
 app.use("/api/auth" , authRoutes)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import { Routes , Route } from 'react-router'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import NotificationsPage from './pages/NotificationsPage'
-import CallPage from './pages/CallPage'
-import ChatPage from './pages/ChatPage'
-import OnBoardingPage from './pages/OnBoardingPage'
-import toast, { Toaster } from 'react-hot-toast'
-
-
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { axiosInstance } from "./lib/axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: "todos",
+    queryFn: async () => {
+      const res = await axiosInstance.get("/me");
+
+      return res.data;
+    },
+  });
+
+  console.log(data);
+  console.log(isLoading);
+  console.log(error);
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/signup' element={<SignupPage/>} />
-      <Route path='/login' element={<LoginPage/>} />
-      <Route path='/notification' element={<NotificationsPage/>} />
-      <Route path='/call' element={<CallPage/>} />
-      <Route path='/chat' element={<ChatPage/>} />
-      <Route path='/onboarding' element={<OnBoardingPage/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notification" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+      </Routes>
 
-    <Toaster/>
+      <Toaster />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
